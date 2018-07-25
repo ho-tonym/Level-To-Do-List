53887a9f41d7b92cc55bb18c9484fa36edec2b59
ho-tonym <ho.tonym@gmail.com>
2018-07-25 13:15:29 -0400

    clean local variables

--- app/assets/javascripts/item.js
+++ app/assets/javascripts/item.js
@@ -1,18 +1,9 @@
 $(function(){
   $("#enter_new_item").submit(function(event) {
-
-    url = this.action
-    //the data sent has to match the controller allows params + db table
-    data = {
-      'authenticity_token': $("input[name='authenticity_token']").attr("value"),
-      'item':{
-        'description': $('.enter_new_item').val()
-      }
-    }
     $.ajax({
       method: "POST",
-      url: url,
-      data: data,
+      url: this.action,
+      data: $(this).serialize(),
       success: function(response){
         $('.enter_new_item').val("")
         let $div = $("div.all-items-inlist")
