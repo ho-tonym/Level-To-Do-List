b6a142f3ecba13f9cb575149a40c556fdc3e5f73
ho-tonym <ho.tonym@gmail.com>
2018-07-26 16:20:54 -0400

    hijack item destroy button

--- app/assets/javascripts/item.js
+++ app/assets/javascripts/item.js
@@ -1,5 +1,6 @@
 $(function(){
-  $("#enter_new_item").submit(function(event) {
+  //forms of that have id of new item
+  $("form#new_item").submit(function(event) {
     $.ajax({
       type: "POST",
       url: this.action,
@@ -13,3 +14,27 @@ $(function(){
     event.preventDefault();
   });
 });
+
+// Item.destroy = function(json){
+//   var item = new Item(json);
+//   item.destroy()
+// }
+// destroy_button
+$(function(){
+  $("form.destroy_button").submit(function(event) {
+    
+    event.preventDefault();
+  });
+});
+
+// $('.col-destroy').on('click', function () {
+//   event.preventDefault();
+//     alert( "Handler for .click() called." );
+// });
+// function(){
+//   $( "button.col-destroy" ).on("click", "input.destroy",function() {
+//     event.preventDefault();
+//     alert( "Handler for .click() called." );
+//   });
+//
+// }
