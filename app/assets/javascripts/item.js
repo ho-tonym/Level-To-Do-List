2372284dbeecabfb91087f1d3326e4e0bede092c
ho-tonym <ho.tonym@gmail.com>
2018-09-28 22:45:40 -0400

    dry up css, put more css on top level so it trickles down.

--- app/assets/javascripts/item.js
+++ app/assets/javascripts/item.js
@@ -50,7 +50,8 @@ $(function(){
         item = new Item(json);
         result = Item.template(item);
         Item.appendToTop(result)
-    }})
+    }
+  })
   });
 });
 
@@ -83,68 +84,5 @@ $(function () {
      });
   });
 });
-//alphabetize
-//hoisting, scope, this context(apply bind, arrow functions)
-
-// static itemCreate1(item){
-//   let templateFunction = Item.template(item);
-//   let result = templateFunction(item)
-//   $("#all_new_items_1").append(result);
-//
-
-// }
-
-// // = require turbolinks
-// $(document).on('turbolinks:load', function (){ alert("turbolinks on load event works") });
-
-// $(function() {
-//   initPage();
-// });
-// $(window).bind('page:change', function() {
-//   initPage();
-// });
-// function initPage() {
-//   // Page ready code...
-// }
-
-/////////////no handlebars//////////////
-// $(function(){
-//   $("form#new_item").submit(function(event) {
-//     event.preventDefault();
-//     $.ajax({
-//       type: "POST",
-//       url: this.action,
-//       data: $(this).serialize(),
-//
-//       success: function(json){
-//         $('.enter_new_item').val("")
-//         let $div = $("div.all-items-inlist")
-//         $div.append(json);
-//     }})
-//   });
-// });
-////////////using handlebars/////////////
-// $(function(){
-//   $("form#new_item").submit(function(event) {
-//     event.preventDefault();
-//
-//     $.ajax({
-//       type: "POST",
-//       url: this.action,
-//       data: $(this).serialize(),
-//       dataType: "json",
-//
-//       success: function(json){
-//       // let item = new Item(json);
-//       // let itemLI = item.renderLI()
-      // $('.enter_new_item').val("")
-      //
-      // source = $("#item-template").html()
-      // template= Handlebars.compile(source);
-      // result = template(json);
-      // $("div.all-items-inlist").append(result);
-//     }})
-//   });
-// });
 
 //when you first arrive on the list show page, when you create a new item on that list, it will show the html
