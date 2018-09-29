da89d8132758ed39160e204bcb8e7d7e7fca9ea0
ho-tonym <ho.tonym@gmail.com>
2018-09-28 22:00:31 -0400

    fix bug when logging out, we would be verufy the current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,9 @@ class ApplicationController < ActionController::Base
   end
 
   def current_user
-    @current_user ||= User.find(session[:user_id])
+    if session[:user_id]
+      @current_user = User.find(session[:user_id])
+    end
   end
 
   def current_user_name
--- config/routes.rb
+++ config/routes.rb
@@ -17,8 +17,5 @@ Rails.application.routes.draw do
     end
   end
 
-
-
-
-  root 'static#index'
+  root 'lists#index'
 end
